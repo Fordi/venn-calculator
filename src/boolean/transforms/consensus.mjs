5822d9f629d503fea861efe39dbaeb0504b8b47a
Bryan Elliott <bryan_elliott@comcast.com>
2022-07-25 19:49:25 -0400

    improved consensus

--- src/boolean/transforms/consensus.mjs
+++ src/boolean/transforms/consensus.mjs
@@ -1,43 +1,45 @@
+import { AND } from "../consts.mjs";
 import { areEqual, contains, isAnd, isOr } from "../tests.mjs";
-import { invert, without } from "../tools.mjs";
+import { findCommon, invert, sortExpr, without } from "../tools.mjs";
 
 // https://en.wikipedia.org/wiki/Consensus_theorem
 // (u (n P Q) (n P !R) (n Q R) ...) => (u (n P !R) (n Q R) ...)
-// The resolvent of (n P !R) and (n Q R) is (n P Q), so it can be removed.
-const testConsensus = (...args) => {
-  const terms = [];
-  let nots = 0;
-  args.forEach(e => {
-    e.slice(1).forEach(x => {
-      if (!terms.some(a => {
-        const neg = areEqual(a, invert(x));
-        if (neg) nots++;
-        return areEqual(a, x) || neg;
-      })) {
-        terms.push(x);
-      }
-    });
+// The resolvant of (n P !R) and (n Q R) is (n P Q), so it can be removed.
+
+const findResolvant = (l, r) => {
+  const exclude = [];
+  const terms = [...l.slice(1), ...r.slice(1)];
+  let hasResolvant = false;
+  terms.forEach(P => {
+    const _P = invert(P);
+    if (contains(r, _P)) {
+      exclude.push(P);
+      exclude.push(_P);
+      hasResolvant = true;
+    }
   });
-  if (nots !== 1) return null;
-  if (terms.length !== 3) return null;
-  // Locate the inverted term...
-  const inv = terms.find(t => args.find(e => contains(e, invert(t))));
-  // Return the arg without the inverted term; that's the one that can be dropped.
-  return args.find(a => !contains(a, inv) && !contains(a, invert(inv)));
+  if (!hasResolvant) return null;
+  const resolvant = new Set(terms.filter(P => !exclude.some((p) => areEqual(p, P))));
+  return sortExpr([AND, ...Array.from(resolvant)]);
 };
 
 const consensus = (exp) => {
   if (!isOr(exp)) return exp;
   let work = exp;
-  const ands = exp.slice(1).filter(isAnd).filter(a => a.length === 3);
+  const ands = exp.slice(1).filter(isAnd);
   if (ands.length < 3) return exp;
-
-  for (let i = 0; i < ands.length - 2; i++) {
-    for (let j = i + 1; j < ands.length - 1; j++) {
-      for (let k = j + 1; k < ands.length; k++) {
-        const remove = testConsensus(ands[i], ands[j], ands[k]);
-        if (remove) {
-          return without(work, remove);
+  const { unique } = findCommon(...ands);
+  const uands = unique ? unique.map((t) => [AND, ...t]) : ands;
+  for (let i = 0; i < uands.length; i++) {
+    for (let j = 0; j < uands.length; j++) {
+      if (i === j) continue;
+      const res = findResolvant(uands[i], uands[j]);
+      if (res) {
+        for (let k = 0; k < uands.length; k++) {
+          if (k === i || k === j) continue;
+          if (areEqual(res, uands[k])) {
+            return without(work, ands[k]);
+          }
         }
       }
     }
